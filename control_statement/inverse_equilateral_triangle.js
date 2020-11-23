eaf7a1858b38a127110c8d6f8f5cf6f8a4ebde53
jiwon <iamkjw77@naver.com>
2020-11-23 11:36:41 +0900

    feat: modified files

--- control_statement/inverse_equilateral_triangle.js
+++ control_statement/inverse_equilateral_triangle.js
@@ -3,7 +3,7 @@ function star(line) {
     let star = '*';
 
     for (let i = 1, j = 1; i < line * 2; i += 2) {
-        queue.push((' ').repeat(line - j) + star.repeat(i) + (' ').repeat(line - j));
+        queue.push((' ').repeat(line - j) + star.repeat(i));
         j += 1;
     }
 
