91ca56e1799eeb2ceb165524873f4592bcdcce4f
iamkjw77 <iamkjw77@naver.com>
2020-12-24 14:40:14 +0900

    refactor: modified files

--- Array_HOF/Q06.js
+++ Array_HOF/Q06.js
@@ -1,13 +1,20 @@
+// 내용 중에 하나라도 바뀌면 주소 값을 바꾸고 싶다.(불변성 유지)
 let todos = [
   { id: 3, content: 'HTML', completed: false },
   { id: 2, content: 'CSS', completed: true },
   { id: 1, content: 'Javascript', completed: false }
 ];
 
+// 방법1
+// function toggleCompletedById(id) {
+//   todos.map(todo => {
+//     if (todo.id === id) todo.completed = !todo.completed;
+//   });
+// }
+
+// 방법2, accessor를 사용하자!
 function toggleCompletedById(id) {
-  todos.map((todo, i) => {
-    if (todo.id === id) todo.completed ? todo.completed = false : todo.completed = true;
-  });
+  todos = todos.map(todo => todo.id === id ? {...todo, completed: !todo.completed } : todo);
 }
 
 toggleCompletedById(2);
--- Array_HOF/Q07.js
+++ Array_HOF/Q07.js
@@ -4,8 +4,14 @@ let todos = [
   { id: 1, content: 'Javascript', completed: false }
 ];
 
+// 방법1
+// function toggleCompletedAll() {
+//   todos.map(todo => { todo.completed = true; });
+// }
+
+// 방법2, 잊지말고 todos에 재할당
 function toggleCompletedAll() {
-  todos.map(todo => { todo.completed = true; });
+  todos = todos.map(todo => ({...todo, completed: true}));
 }
 
 toggleCompletedAll();
