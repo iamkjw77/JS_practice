759f96d83dafdac49bffcd7a4af2ab3e09701c93
jiwon <iamkjw77@naver.com>
2020-12-26 01:59:13 +0900

    feat: add files

---
+++ algorithm/insertionSort.js
@@ -0,0 +1,20 @@
+function insertionSort(array) {
+  let key;
+  let j;
+
+  for (let i = 1; i < array.length; i++) {
+    key = array[i];
+    j = i - 1;
+
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
+  return array;
+}
+
+console.log(insertionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(insertionSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+console.log(insertionSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
---
+++ algorithm/selectionSort.js
@@ -0,0 +1,20 @@
+function selectionSort(array) {
+  let min;
+  let tmp;
+
+  for (let i = 0; i < array.length; i++) {
+    min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[min] > array[j]) min = j;
+    }
+    tmp = array[i];
+    array[i] = array[min];
+    array[min] = tmp;
+  }
+  
+  return array;
+}
+
+console.log(selectionSort([3, 1, 0, -1, 4, 2])); // [-1, 0, 1, 2, 3, 4]
+console.log(selectionSort([2, 4, 5, 1, 3]));     // [1, 2, 3, 4, 5]
+console.log(selectionSort([5, 2, 1, 3, 4, 6]));  // [1, 2, 3, 4, 5, 6]
